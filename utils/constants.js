9d8f1224caa61ad51b76eed53f24b22dfa4d4a73
igorsheg-wix <igors@wix.com>
2019-09-23 12:25:37 +0300

    Changed intervals

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 const intervals = {
-	refetch: 3.6e6, // One Hour
+	refetch: 15 * 60 * 1000, // 15 min
 	projects: 3 * 60 * 1000,
 	collections: 60 * 1000,
 	previews: 20 * 1000
@@ -10,8 +10,8 @@ const details = {
 };
 
 const limits = {
-	projects: 20, // No point of going higher since the app will refetch new 20 projects after refetch interval exactly at the end of the old 20
-	collections: 2,
-	previews: 2
+	projects: 5, // No point of going higher since the app will refetch new 20 projects after refetch interval exactly at the end of the old 20
+	collections: 3,
+	previews: 3
 };
 export { intervals, limits, details };
